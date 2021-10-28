244e44a3af71dd0848d8f186c72b34ef5387ae24
bimaaryafatah <bimaaryafatah23@gmail.com>
2021-10-28 20:02:13 +0700

    first commit

---
+++ src/main/kotlin/PlugInProject1.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+        var Nama : String = "Bima Arya Fatah"
+        var NIM : String = "21090029"
+        var TTL : String = "23 gustus 2003"
+        var Prodi : String = "Teknik Informatika"
+
+        println(Nama)
+        println(NIM)
+        println(TTL)
+        println(Prodi)
+        println(Nama.length)
+        println(NIM.length)
+        println(TTL.length)
+        println(Prodi.length)
+}
